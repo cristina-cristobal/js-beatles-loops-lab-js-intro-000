e1441584f7ada70409eac14f931f51d95222aa00
imperative-architecture-1668 <cristina.cristobal@gmail.com>
2018-12-12 23:53:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // add solution here
+function theBeatlesPlay(musician, instrument) {
+  var musician = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+  var instrument = ["guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+}
